df2473f4aea51838648820211dfca0b548fbf015
Ali Alshehri <ps4ps5cc@gmail>
2026-04-12 06:18:56 +0300

    add base template with navbar,footer

--- SaqrEye/main/urls.py
+++ SaqrEye/main/urls.py
@@ -2,18 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ─── Persona ──────────────────────────────────────────────────────────────
-    path('', views.persona_view, name='persona_view'),
+    # - - - Persona - - -
+    path('', views.persona_view, name='persona_view'), #first page
     path('change-persona/', views.change_persona_view, name='change_persona_view'),
 
-    # ─── Language ─────────────────────────────────────────────────────────────
+    # - - -  Language - - - 
     path('set-language/', views.set_language_view, name='set_language_view'),
 
-    # ─── Main Pages ───────────────────────────────────────────────────────────
+    # - - - - Main Pages - - - -
     path('home/', views.home_view, name='home_view'),
     path('about/', views.about_view, name='about_view'),
 
-    # ─── Cities & Items ───────────────────────────────────────────────────────
+    # - - - - Cities & Items - - - -
     path('city/<str:city_slug>/', views.city_detail_view, name='city_detail_view'),
     path('city/<str:city_slug>/<str:item_type>/<int:item_id>/', views.item_detail_view, name='item_detail_view'),
 ]
